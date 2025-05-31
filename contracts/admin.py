9560c083d222623895e1042baeffc9a8e8f41e77
alabri0803 <rashid92759346@gmail.com>
2025-05-31 20:28:14 +0000

    ad

--- contracts/admin.py
+++ contracts/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Contract, Invoice
 
-admin.site.register(Contract)
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+  list_display = ('tenant', 'unit', 'start_date', 'end_date', 'monthly_rent', 'admin_fees', 'total_amount')
+  search_fields = ('tenant__full_name', 'unit__unit_number')
+  list_filter = ('start_date', 'end_date')
 admin.site.register(Invoice)
